bb9993930f8b5ee73853533414559fb333a6e26e
Guoguo Lu <snowsguoguo@gmail.com>
2014-06-07 14:51:21 -0700

    LRU Cache

---
+++ leetcode/src/test/java/com/leetcode/LRUCacheTest.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+import junit.framework.TestCase;
+
+public class LRUCacheTest extends TestCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
