ceeed9cc6b7d3bc05db95f0d9ddbd49826f11f54
Fiona Lu <snowsguoguo@gmail.com>
2015-04-15 14:40:06 -0700

    HW1 exercise 2

--- ScalaCoursera/recfun/src/main/scala/recfun/Main.scala
+++ ScalaCoursera/recfun/src/main/scala/recfun/Main.scala
@@ -14,12 +14,28 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) 1
+    else pascal(c-1, r-1) + pascal(c, r-1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    if (chars.isEmpty) false
+    var leftParenthesis = 0
+    for (c <- chars) {
+      if (c == '(') leftParenthesis += 1
+      else if (c == ')') {
+        if (leftParenthesis < 1) false
+        else leftParenthesis -= 1
+      }
+    }
+
+    if (leftParenthesis == 0) true
+    false
+  }
 
   /**
    * Exercise 3
