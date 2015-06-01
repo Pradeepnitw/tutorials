5d17310f1e001d7e8525047698e7fcd00fafbc77
Fiona Lu <glu@livongo.com>
2015-06-01 16:21:56 -0700

    May2015 Leetcodes

---
+++ leetcode/src/main/java/leetcode/may2015/JumpGame.java
@@ -0,0 +1,17 @@
+package leetcode.feb2015;
+
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
+        
+        int reach = 0;
+        int i = 0;
+        
+        while (i < nums.length && i <= reach) {
+            reach = Math.max(nums[i] + i, reach);
+            i++;
+        }
+        if (i == nums.length) return true;
+        else return false;
+    }
+}
